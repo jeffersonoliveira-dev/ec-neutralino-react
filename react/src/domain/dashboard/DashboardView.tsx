68076bc4d15384ac3a2b341ec33d92a6f4bbc568
Jefferson Oliveira <jeffersonoliveiradev@gmail.com>
2023-01-26 21:08:58 -0300

    fix: module path

--- react/src/domain/dashboard/DashboardView.tsx
+++ react/src/domain/dashboard/DashboardView.tsx
@@ -1,20 +1,9 @@
 import React, { useEffect } from "react";
 import { Div } from "./Dashboard.styled";
-import { fetch } from "./DashboardMate"
 
 
 export const Dashboard =  () => {
 
-    const handleFetch = async () => {
-        /// brincadeira
-        const result = await fetch()
-    }
-
-    useEffect(() => {
-        handleFetch()
-    }, [])
-
-
-    return <Div/>
+    return <Div>ola</Div>
 }
 
